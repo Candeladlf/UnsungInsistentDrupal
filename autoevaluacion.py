0742fb7789c0aba3b917ceee1c5f554848f129b2
JFdelaFuente <josefcodelafuente@gmail.com>
2020-10-21 07:57:57 +0000

    Incluido comentarios

--- autoevaluacion.py
+++ autoevaluacion.py
@@ -1,37 +1,32 @@
-kilometros = float(input("¿Cuandos kilometros ha recorrido? "))
-litros = float(input("¿Cuantos litros ha consumido? "))
-print(f"El consumo ha sido : {round(100*litros/kilometros,2)}  litros cada 100 kms")
-print("\n")
+# Autoevalucuacion - Tema 1
 
-IVA = float(input("¿Cuanto IVA vas a pagar (sin porcentaje) ? "))
-precio = float(input("¿Cuanto IVA vas a pagar (sin porcentaje) ? "))
-print("El precio final es " +str((IVA/100)*precio+precio))
-print("\n")
-
+# Ejercicio 1
 import math
 print(round(2 * 3 / math.pi, 2))
 print(round(4*3*math.pi/2))
 print(abs(0.01 * (-253) * math.pi))
 print(round(4*3**2 * math.pi/2))
 
-
+# Ejercicio 2
 lado1 = float(input("introduce lado 1 rectángulo : "))
 lado2 = float(input("introduce lado 2 rectángulo : "))
 print("El perímetro del rectángulo es : " +str(2*lado1 + 2*lado2)+ " metros")
 print("El área del rectángulo es : " +str(lado1*lado2)+ " m2")
 print("\n")
 
+# Ejercicio 3
 base = float(input("introduce base triangulo : "))
 altura = float(input("introduce altura triangulo : "))
 print("El área del triangulo es " +str(base*altura/2)+ " m2")
 print("\n")
 
-import math
+# Ejercicio 4
 d = float(input("Introduce diametro del circulo : "))
 print("El longitud del circulo es de " +str(round(d*math.pi, 2))+ " metros")
 print("El área del circulo es " +str(round(math.pi * (d/2)**2, 2))+ " m2")
 print("\n")
 
+# Ejercicio 5
 valor=float(input("Introduce un valor con dos decimales: "))
 print("la raiz cuadrada es: " +str(round(math.sqrt(valor),2)))
 print("\n")
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
+# Actividad 2 - Ejercicio 3
 kilometros = float(input("¿Cuandos kilometros ha recorrido? "))
 litros = float(input("¿Cuantos litros ha consumido? "))
 print(f"El consumo ha sido : {round(100*litros/kilometros,2)}  litros cada 100 kms")
-
+# Actividad 2 - Ejercicio 4
 IVA = float(input("¿Cuanto IVA vas a pagar (sin porcentaje) ? "))
 precio = float(input("¿Cuanto IVA vas a pagar (sin porcentaje) ? "))
 print("El precio final es " +str((IVA/100)*precio+precio))
