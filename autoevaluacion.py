401d000761316a8b1b03a5cef62da6f39ce5980b
JFdelaFuente <josefcodelafuente@gmail.com>
2020-10-21 06:50:11 +0000

    saltos de linea

--- autoevaluacion.py
+++ autoevaluacion.py
@@ -1,18 +1,37 @@
+kilometros = float(input("¿Cuandos kilometros ha recorrido? "))
+litros = float(input("¿Cuantos litros ha consumido? "))
+print(f"El consumo ha sido : {round(100*litros/kilometros,2)}  litros cada 100 kms")
+print("\n")
 
-
-kilometros = float(input("¿Cuandos kilometros ha recorrido?"))
-litros = float(input("¿Cuantos litros ha consumido?"))
-print("El consumo ha sido :" +str(100*litros/kilometros)+ " litros cada 100 kms")
-
-print("¿Cuanto IVA vas a pagar (sin porcentaje) ?")
-IVA = float(input())
-print("¿Cuanto cuesta el producto sin IVA?")
-precio = float(input())
+IVA = float(input("¿Cuanto IVA vas a pagar (sin porcentaje) ? "))
+precio = float(input("¿Cuanto IVA vas a pagar (sin porcentaje) ? "))
 print("El precio final es " +str((IVA/100)*precio+precio))
-
+print("\n")
 
 import math
 print(round(2 * 3 / math.pi, 2))
 print(round(4*3*math.pi/2))
 print(abs(0.01 * (-253) * math.pi))
 print(round(4*3**2 * math.pi/2))
+
+
+lado1 = float(input("introduce lado 1 rectángulo : "))
+lado2 = float(input("introduce lado 2 rectángulo : "))
+print("El perímetro del rectángulo es : " +str(2*lado1 + 2*lado2)+ " metros")
+print("El área del rectángulo es : " +str(lado1*lado2)+ " m2")
+print("\n")
+
+base = float(input("introduce base triangulo : "))
+altura = float(input("introduce altura triangulo : "))
+print("El área del triangulo es " +str(base*altura/2)+ " m2")
+print("\n")
+
+import math
+d = float(input("Introduce diametro del circulo : "))
+print("El longitud del circulo es de " +str(round(d*math.pi, 2))+ " metros")
+print("El área del circulo es " +str(round(math.pi * (d/2)**2, 2))+ " m2")
+print("\n")
+
+valor=float(input("Introduce un valor con dos decimales: "))
+print("la raiz cuadrada es: " +str(round(math.sqrt(valor),2)))
+print("\n")
--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 
-lado1 = float(input("introduce lado 1 rectángulo : "))
-lado2 = float(input("introduce lado 2 rectángulo : "))
+kilometros = float(input("¿Cuandos kilometros ha recorrido? "))
+litros = float(input("¿Cuantos litros ha consumido? "))
+print(f"El consumo ha sido : {round(100*litros/kilometros,2)}  litros cada 100 kms")
 
-print("El perímetro del rectángulo es : " +str(2*lado1 + 2*lado2)+ " metros")
-print("El área del rectángulo es : " +str(lado1*lado2)+ " m2")
-print("\n")
-
-base = float(input("introduce base triangulo : "))
-altura = float(input("introduce altura triangulo : "))
-
-print("El área del triangulo es " +str(base*altura/2)+ " m2")
-print("\n")
-
-import math
-d = float(input("Introduce diametro del circulo : "))
-
-print("El longitud del circulo es de " +str(round(d*math.pi, 2))+ " metros")
-print("El área del circulo es " +str(round(math.pi * (d/2)**2, 2))+ " m2")
-print("\n")
-
-valor=float(input("Introduce un valor con dos decimales: "))
-print("la raiz cuadrada es: " +str(round(math.sqrt(valor),2)))
-print("\n")
+IVA = float(input("¿Cuanto IVA vas a pagar (sin porcentaje) ? "))
+precio = float(input("¿Cuanto IVA vas a pagar (sin porcentaje) ? "))
+print("El precio final es " +str((IVA/100)*precio+precio))
